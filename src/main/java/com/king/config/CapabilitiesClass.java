7a8161e771d0043f99da23d128e4fe70c08ddabc
Nitun Pachauri <pachnitu@amazon.com>
2022-02-09 01:29:08 +0530

    Added a valid .apk and path capability

--- src/main/java/com/king/config/CapabilitiesClass.java
+++ src/main/java/com/king/config/CapabilitiesClass.java
@@ -2,18 +2,20 @@ package com.king.config;
 
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import java.io.File;
 
 
 public class CapabilitiesClass {
 
     // To set up the driver capabilities for android
     public DesiredCapabilities driverCapabilitiesAndroid() {
-
+        File app= new File("apps/CandyCrushSaga.apk");
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
         caps.setCapability("VERSION", "10");
         caps.setCapability("appPackage", "com.king.candycrushsaga");
         caps.setCapability("appActivity", "com.king.candycrushsaga.CandyCrushSagaActivity");
+        caps.setCapability("app", app.getAbsolutePath());
         caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiAutomator2");
 
         return caps;
